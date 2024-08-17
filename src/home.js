85da38bfd689c2424a144017208b250c9a4a8694
Satyam Kumar <113851151+SKumr20@users.noreply.github.com>
2024-08-17 21:52:09 +0530

    modified:   src/home.js

--- src/home.js
+++ src/home.js
@@ -1,4 +1,4 @@
 export function loadContent() {
     const contentDiv = document.getElementById('content');
-    contentDiv.innerHTML = '<h1>Welcome to the Home Page</h1><h2>This is your one stop for all things music!</h2>';
+    contentDiv.innerHTML = '<h1>Welcome, to the Home Page</h1><h2>This is your one stop for all things music!</h2>';
 }
